b5e0721d7b312a382aa037954278f15be5ce8e60
Dazzla <dazzlebown@gmail.com>
2018-01-17 14:07:18 +0000

    Adds resource specs

--- specs/resource_spec.rb
+++ specs/resource_spec.rb
@@ -1,55 +1,52 @@
 require_relative '../specs/spec_helper'
 
-RSpec.describe('Users') do
+RSpec.describe('Resources') do
 
-  #TODO: This spec could be better. Needs to be tidied to reduce repetition and improve readability
-
-  let :user_fields do
-    [:id, :first_name, :last_name, :avatar]
+  let :resource_fields do
+    [:id, :name, :year, :color, :pantone_value]
   end
 
-  describe 'list_users' do
+  describe 'list_resources' do
 
     let :url do
-      BASE_URL + '/users/?page=2'
+      BASE_URL + '/unknown'
     end
 
-    it 'retrieves the user list' do
+    it 'retrieves the resource list' do
       response = RestClient.get(url)
       response_body = JSON.parse(response)
       expect(response.code).to eq 200
       expect(response_body.has_key? 'data')
-      expect(response_body['page']).to eq 2
       expect(response_body['data'][0].length).to be > 1
-      user_fields.each do |field|
+      resource_fields.each do |field|
         expect(response_body['data'][0].has_key? field)
       end
     end
   end
 
-  describe 'retrieve single user' do
+  describe 'retrieve single resource' do
 
     let :url do
-      BASE_URL + '/users/2'
+      BASE_URL + '/unknown/2'
     end
 
-    it 'retrieves a single user' do
+    it 'retrieves a single resource' do
       response = RestClient.get(url)
       response_body = JSON.parse(response)
       expect(response.code).to eq 200
       expect(response_body.has_key? 'data')
-      user_fields.each do |field|
+      resource_fields.each do |field|
         expect(response_body.has_key? field)
       end
     end
   end
 
-  describe 'single user not found' do
+  describe 'single resource not found' do
     let :url do
-      BASE_URL + '/users/2888'
+      BASE_URL + '/unknown/2888'
     end
 
-    it 'returns an error for a non-existent user record' do
+    it 'returns an error for a non-existent resource' do
       RestClient.get(url) do |response, request, result|
         expect(response.code).to eq 404
         expect(response.empty?)
@@ -57,5 +54,6 @@ RSpec.describe('Users') do
     end
 
   end
+
 
 end
